f20a1bf46e56b9d57c42f06687f2381e1055faff
Jacob <jacob.beallor@queensu.ca>
2020-04-15 16:23:42 -0400

    clean up

--- evaluation.py
+++
@@ -1,9 +0,0 @@
-import sklearn
-from linear-interp import img as im_test
-from linear-interp import image as im_true
-
-# peak signal to noise ratio
-skimage.measure.compare_psnr(im_true, im_test, data_range=None)
-
-# SSIM
-skimage.measure.compare_ssim(X, Y, win_size=None, gradient=False, data_range=None, multichannel=False, gaussian_weights=False, full=False, **kwargs)
